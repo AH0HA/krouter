8a0384862093ea337c45372bb24aa5806dd57937
makayo <kayama818@gmail.com>
2016-09-26 23:24:40 +0000

    mem & config is working

--- file2hash.pl
+++ file2hash.pl
@@ -2,6 +2,12 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use POSIX qw(strftime);
+
+
+#
+#file2hash : read the file in k<file_name> e.g.=kconfig &  kmem  into hash table
+#
 
 sub file2hash {
     my ($file) = @_;
@@ -32,23 +38,96 @@ while ( <$data>) {
     return %HoH;
 
 }
+#
+#end file2hash
+#
 
+
+#
+#print out hash table in k<file_name> format
+#
 sub hash2print{
-    my (%HoH) = @_;
+    (my %HoH,my $debug) = @_;
+    #my ($debug)=@_||0;
+    #my %HoH = shift;
+    #my $debug = shift || 0;
 
-my $family;
-my $role;
+    my $family;
+    my $role;
 
-#if ($debug) {
-#to_do make a subroutine and add debug flag
     for $family ( keys %HoH ) {
-        print "$family: ";
+        #print "$family\n";
         for $role ( keys %{ $HoH{$family} } ) {
-             print "$role=$HoH{$family}{$role} ";
+             if ($debug){
+                print "family:$family\n";
+                print "role: $role\n";
+             }
+             print "$role=$HoH{$family}{$role}";
+
         }
         print "\n";
     }
 }
+#
+#end hash2print
+#
+
+sub dispatch{
+
+        my $event= shift;
+        my $debug = shift||0;
+        my $config_f = shift || "kconfig";
+        my $memory_f = shift || "kmem";
+
+        my %h2=&file2hash($config_f);
+        my %m2=file2hash($memory_f);
+
+        my $today=&getDate();
+
+        if ($debug){
+            print "$today\n";
+            print "$event\n";
+            print "$config_f\n";
+            print "$memory_f\n";
+        }
+
+        my $ctag1="email1";
+        my $ctag2="email1_cnt";
+        my $ctag3="email2";
+
+        if ($debug){
+            print "$h2{$event}{$ctag1}\n";
+            print "$h2{$event}{$ctag3}\n";
+            print "$h2{$event}{$ctag2}\n";
+            print "$m2{$today}{$event}\n";
+        }
+
+        #my $inc_cnt = $m2{$today}{$inc} || -999999999;
+        #print "$inc_cnt\n";
+
+        #my %config = shift;
+        #my %mem = shift;
+        #my $event = shift;
+        #print $m2{$inc}{$today};
+}
+
+sub getDate{
+my $date = strftime "%Y%m%d", localtime;
+#print $date;
+return $date;
+}
 
 my %h2=&file2hash("kconfig");
-&hash2print(%h2);
+my %m2=&file2hash("kmem");
+#hash2print(%h2);
+#hash2print(%m2);
+#print &getDate();
+#my $xcnt= &dispatch("event_c3_z2");
+&dispatch("event_c3_z2");
+#print $xcnt;
+
+
+
+
+
+
