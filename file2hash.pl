3fec155658ebe5fa081301dceb06e5f9260c24eb
makayo <kayama818@gmail.com>
2016-09-27 00:58:51 +0000

    write file is working and add key to hash yet to work and update hash key-value need to test

--- file2hash.pl
+++ file2hash.pl
@@ -5,10 +5,17 @@ use Getopt::Long;
 use POSIX qw(strftime);
 
 
+#getDate return date in string format 20161010
+sub getDate{
+my $date = strftime "%Y%m%d", localtime;
+#print $date;
+return $date;
+}
+#end getDate
+
 #
 #file2hash : read the file in k<file_name> e.g.=kconfig &  kmem  into hash table
 #
-
 sub file2hash {
     my ($file) = @_;
 
@@ -42,12 +49,14 @@ while ( <$data>) {
 #end file2hash
 #
 
-
 #
+#hash2print
 #print out hash table in k<file_name> format
 #
 sub hash2print{
-    (my %HoH,my $debug) = @_;
+    (my $hashref,my $debug) = @_;
+    my %HoH = %$hashref;
+
     #my ($debug)=@_||0;
     #my %HoH = shift;
     #my $debug = shift || 0;
@@ -59,7 +68,7 @@ sub hash2print{
         #print "$family\n";
         for $role ( keys %{ $HoH{$family} } ) {
              if ($debug){
-                print "family:$family\n";
+               print "family:$family\n";
                 print "role: $role\n";
              }
              print "$role=$HoH{$family}{$role}";
@@ -72,10 +81,45 @@ sub hash2print{
 #end hash2print
 #
 
+#
+#hash2file2
+#print out hash table in k<file_name> format
+#
+sub hash2file2{
+    #(my %HoH,my $debug) = @_;
+    (my $hashref,my $fh) = @_;
+    my %HoH = %$hashref;
+    #my %HoH = shift;
+    #my $fh = shift;
+    #my $debug = shift ||0;
+
+    my $family;
+    my $role;
+
+    for $family ( keys %HoH ) {
+        #print "$family\n";
+        for $role ( keys %{ $HoH{$family} } ) {
+             #if ($debug){
+              #  print $fh "family:$family\n";
+               # print $fh "role: $role\n";
+             #}
+             print $fh "$role=$HoH{$family}{$role}";
+
+        }
+        print $fh "\n";
+    }
+
+    close $fh;
+}
+#
+#end hash2file2
+#
+
 sub dispatch{
 
         my $event= shift;
         my $debug = shift||0;
+        my $mail_prog = shift || "mailx";
         my $config_f = shift || "kconfig";
         my $memory_f = shift || "kmem";
 
@@ -89,41 +133,65 @@ sub dispatch{
             print "$event\n";
             print "$config_f\n";
             print "$memory_f\n";
+            print "$mail_prog\n";
         }
 
-        my $ctag1="email1";
-        my $ctag2="email1_cnt";
-        my $ctag3="email2";
+        my $email1_tag="email1";
+        my $email1_cnt_tag="email1_cnt";
+        my $email2_tag="email2";
+
+        my $xemail1 = $h2{$event}{$email1_tag};
+        my $xemail2 = $h2{$event}{$email2_tag};
+        my $xemail1_cnt = $h2{$event}{$email1_cnt_tag};
+
+        #initialize today_event_cnt is not happened today
+        my $today_event_cnt = $m2{$today}{$event}||1;
+        if ($today_event_cnt  == 1){
+            $m2{$today}{$event} = 1;
+        }
 
         if ($debug){
-            print "$h2{$event}{$ctag1}\n";
-            print "$h2{$event}{$ctag3}\n";
-            print "$h2{$event}{$ctag2}\n";
-            print "$m2{$today}{$event}\n";
+            print "$xemail1\n";
+            print "$xemail2\n";
+            print "$xemail1_cnt\n";
+            print "$today_event_cnt\n";
         }
 
-        #my $inc_cnt = $m2{$today}{$inc} || -999999999;
-        #print "$inc_cnt\n";
+        my $mail1_cmd_str = $mail_prog." -s ".$event." ".$xemail1;
+        my $mail2_cmd_str = $mail_prog." -s ".$event." ".$xemail2;
 
-        #my %config = shift;
-        #my %mem = shift;
-        #my $event = shift;
-        #print $m2{$inc}{$today};
-}
+        if ($today_event_cnt + 1 >$xemail1_cnt){
 
-sub getDate{
-my $date = strftime "%Y%m%d", localtime;
-#print $date;
-return $date;
-}
+            system "$mail2_cmd_str";
 
-my %h2=&file2hash("kconfig");
-my %m2=&file2hash("kmem");
+        }else{
+
+            system "$mail1_cmd_str";
+
+        }
+
+
+        #hash2print(\%m2);
+        #open my $rm3d_fh, '>', $memory_f or die "...$!";
+        #hash2file2(\%m2,$rm3d_fh);
+        hash2print(\%m2);
+
+
+
+
+
+}#end dispatch
+
+
+
+#my %h2=&file2hash("kconfig");
+#my %m2=&file2hash("kmem");
 #hash2print(%h2);
 #hash2print(%m2);
 #print &getDate();
 #my $xcnt= &dispatch("event_c3_z2");
-&dispatch("event_c3_z2");
+&dispatch("event_c3_z2",1,"echo");
+#&dispatch("event_c3_z2",1);
 #print $xcnt;
 
 
