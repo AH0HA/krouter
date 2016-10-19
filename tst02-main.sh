cb19fbb8ad57ddaed04e8c81d4278075ed7fb02f
nericKa <nericlau@gmail.com>
2016-10-18 17:40:42 -0700

    -a working

--- tst02-main.sh
+++ tst02-main.sh
@@ -1,6 +1,6 @@
 #cat kmem
 #./krouter -s "event_a1_x1" -verbose
 #./krouter -s "event_c3_z2" -verbose
-./krouter -s "event_a1_x1" 
-./krouter -s "event_c3_z2" 
+./krouter -s "event_a1_x1" -a "./to_do"
+./krouter -s "event_c3_z2"  -a "./to_do"
 #cat kmem
